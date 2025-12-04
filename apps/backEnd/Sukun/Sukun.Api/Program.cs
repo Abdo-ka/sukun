aa01b98940a26c2805cb3768cc0cceb922d9d4cd
Moner <monernashed3@gmail.com>
2025-12-04 18:23:10 +0300

    clear and readd cache

--- apps/backEnd/Sukun/Sukun.Api/Program.cs
+++ apps/backEnd/Sukun/Sukun.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Sukun.Infrastructure.Context;
+
 namespace Sukun.Api
 {
     public class Program
@@ -13,6 +16,7 @@ namespace Sukun.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
